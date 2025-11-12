69bccc60762962aef0846369d30b9bf6700ace3c
Robin Lugo Boero <rjlb991102@hotmail.com>
2025-11-12 11:15:56 -0500

    chore: Formateo de Query Nativa

--- src/main/java/com/robinlb99/pruebatecnica/feature/employee/repository/EmployeeRepository.java
+++ src/main/java/com/robinlb99/pruebatecnica/feature/employee/repository/EmployeeRepository.java
@@ -9,7 +9,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     @Query(
-        "SELECT e FROM Employee e WHERE e.salary > (SELECT AVG(emp.salary) FROM Employee emp) ORDER BY e.salary DESC"
+        "SELECT e "
+        + "FROM Employee e "
+        + "WHERE e.salary > (SELECT AVG(emp.salary) "
+        					+ "FROM Employee emp) "
+        + "ORDER BY e.salary DESC"
     )
     public List<Employee> getAboveAverageSalaryEmployees();
 }
